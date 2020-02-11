d47c569ffefde4172e399cef0772a586d9e78266
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2020-02-11 13:44:46 -0700

    python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+import time
+import datetime
+import picamera
+
+print "okay just a moment"
+date_time = datetime.datetime.now()
+str_today = str(date_time)
+time.sleep(15)
+camera = picamera.PiCamera()
+camera.capture('Garagedoor.jpg')
+print "Here's your picture" + " Time : " + str_today
