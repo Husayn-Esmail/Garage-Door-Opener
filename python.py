c7cf069fa95839aebed6ad48da623d461cac27e4
Husayn <husayn@esmail.ca>
2020-02-26 21:24:13 -0700

    attempts at optimization

--- python.py
+++ python.py
@@ -2,10 +2,12 @@ import time
 import datetime
 import picamera
 
-print "okay just a moment"
+
+print ("okay just a moment")
 date_time = datetime.datetime.now()
 str_today = str(date_time)
 time.sleep(15)
 camera = picamera.PiCamera()
 camera.capture('Garagedoor.jpg')
-print "Here's your picture" + " Time : " + str_today
+print ("Here's your picture" + " Time : " + str_today)
+
